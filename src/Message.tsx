854cb33be6fb6bba227e7fb7a837f1385badf607
iamjayenmthakuri <malla.jayen@gmail.com>
2023-03-30 16:19:33 +0545

    first commit

---
+++ src/Message.tsx
@@ -0,0 +1,8 @@
+// PascalCasing- capatilize of every word
+function Message() {
+  // jsx : javascript xml
+  const name = "";
+  if (name) return <h1>Hello {name}</h1>;
+  return <h1>Hello World</h1>;
+}
+export default Message;
